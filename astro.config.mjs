d02b499dbe97a235ea0e2cca085855a52049e143
xaqq <xaqq@xaqq.xaqq>
2026-05-06 11:48:01 +0000

    fix: combine tailwind and sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bogorbet.netlify.app',
   integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
