9e464ed911be1c0baecf3be0d8c32b5276b431b4
xaqq <xaqq@xaqq.xaqq>
2026-05-06 11:33:31 +0000

    update konfigurasi sitemap dan robots bogorbet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://bogorbet.netlify.app',
+  integrations: [sitemap()],
 });
